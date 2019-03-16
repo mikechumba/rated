0848a88cdeed616e8becc0bffdfc688fde392415
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-16 14:16:59 +0300

    create projects model

--- rate/models.py
+++ rate/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+   '''
+   Holds user's profile data.
+   '''
 
    user = models.OneToOneField(User, on_delete=models.CASCADE)
    avatar = models.ImageField(upload_to='avatars/')
@@ -11,4 +14,14 @@ class Profile(models.Model):
    def __str__(self):
       return self.user.username
 
+class Project(models.Model):
+   '''
+   To hold user's project data.
+   '''
 
+   author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+   name = models.CharField(max_length=70)
+   description = models.TextField(max_length=140)
+   img = models.ImageField(upload_to='projects/')
+   time_published = models.DateField(auto_now=True)
+   
