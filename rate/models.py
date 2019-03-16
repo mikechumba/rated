4fff26cb6ee6deb31a75b7ccfb4b04705d9a8d52
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-16 14:08:32 +0300

    create profile model

--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   avatar = models.ImageField(upload_to='avatars/')
+   bio = models.TextField(max_length=140)
+
+   def __str__(self):
+      return self.user.username
+
+
