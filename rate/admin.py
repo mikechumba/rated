9e27e3118681486af83fe05be1887ea1b19131b0
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-16 16:10:09 +0300

    register models

--- rate/admin.py
+++ rate/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Profile,Project,Language,Rating,Contact
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Language)
+admin.site.register(Rating)
+admin.site.register(Contact)
