b8b006ec255d36714de64ce37f5a28c8bbd157f7
Michael Kipchumba <michaelchumba09@gmail.com>
2019-03-20 08:10:42 +0300

    update tests

--- rate/tests.py
+++ rate/tests.py
@@ -7,24 +7,24 @@ def create_user_instance():
    user = User(username='johndoe',email='john@email.com',first_name='John',last_name='Doe',password='password')
    return user
 
-def create_profile_instance():
-   profile = Profile(user_id=1,avatar='default.jpg',bio="This is a bio.")
+def create_profile_instance(user):
+   profile = Profile(user=user,avatar='default.jpg',bio="This is a bio.")
    return profile
 
-def create_project_instance():
-   project = Project(name="A Project",description="Yep! It is a project.",img="default.jpg",link="aproject.com")
+def create_project_instance(prfl):
+   project = Project(name="A Project",description="Yep! It is a project.",img="default.jpg",link="aproject.com",author=prfl)
    return project
 
 def create_language_instance():
    language = Language(language='Python')
    return language
 
-def create_rating_instance():
-   review = Rating(review='A review',design=10,usability=10,content=10,rated_id=1,rated_by_id=1)
+def create_rating_instance(project,prfl):
+   review = Rating(review='A review',design=10,usability=10,content=10,rated=project,rated_by=prfl)
    return review
 
-def create_contact_instance():
-   contact = Contact(facebook="fb",twitter="twtr",instagram="insta",github="git",prfl_id=1)
+def create_contact_instance(prfl):
+   contact = Contact(facebook="fb",twitter="twtr",instagram="insta",github="git",prfl=prfl)
    return contact
 
 class UserTest(TestCase):
@@ -44,14 +44,14 @@ class ProfileTest(TestCase):
 
    def setUp(self):
       self.new_user = create_user_instance()
-      self.new_profile = create_profile_instance()
+      self.new_user.save()
+      self.new_profile = create_profile_instance(self.new_user)
 
    def test_profile_instance(self):
       self.assertTrue(isinstance(self.new_profile,Profile))
 
    def test_save_profile(self):
       self.new_profile.save()
-      self.new_user.save()
       profile = Profile.objects.all()
       self.assertTrue(len(profile),1)
 
@@ -63,35 +63,63 @@ class LanguageTest(TestCase):
    def test_language_instance(self):
       self.assertTrue(isinstance(self.new_language,Language))
 
+   def test_save_language(self):
+      self.new_language.save()
+      languages = Language.objects.all()
+      self.assertTrue(len(languages),1)
+
 class ProjectTest(TestCase):
 
    def setUp(self):
-      self.new_project = create_project_instance()
+      self.new_language = create_language_instance()
+      self.new_language.save()
+      self.new_user = create_user_instance()
+      self.new_user.save()
+      self.new_profile = create_profile_instance(self.new_user)
+      self.new_profile.save()
+      self.new_project = create_project_instance(self.new_profile)
 
    def test_project_instance(self):
       self.assertTrue(isinstance(self.new_project,Project))
 
+   def test_save_project(self):
+      self.new_project.save()
+      self.new_project.language.add(self.new_language)
+      projects = Project.objects.all()
+      self.assertTrue(len(projects),1)
+
 class ContactTest(TestCase):
 
    def setUp(self):
-      self.profile = create_profile_instance()
-      self.user = create_user_instance()
-      self.new_contact = create_contact_instance()
+      self.new_user = create_user_instance()
+      self.new_user.save()
+      self.profile = create_profile_instance(self.new_user)
+      self.profile.save()
+      self.new_contact = create_contact_instance(self.profile)
 
    def test_contact_instance(self):
       self.assertTrue(isinstance(self.new_contact,Contact))
 
    def test_save_contact(self):
       self.new_contact.save()
-      # self.profile.save()
-      # self.user.save()
       contacts = Contact.objects.all()
       self.assertTrue(len(contacts),1)
 
 class RatingTest(TestCase):
 
    def setUp(self):
-      self.new_rating = create_rating_instance()
+      self.new_user = create_user_instance()
+      self.new_user.save()
+      self.new_profile = create_profile_instance(self.new_user)
+      self.new_profile.save()
+      self.new_project = create_project_instance(self.new_profile)
+      self.new_project.save()
+      self.new_rating = create_rating_instance(self.new_project,self.new_profile)
 
    def test_rating_instance(self):
       self.assertTrue(isinstance(self.new_rating,Rating))
+
+   def test_save_rating(self):
+      self.new_rating.save()
+      ratings = Rating.objects.all()
+      self.assertTrue(len(ratings),1)
